501f4046b9720606febc05048796d7b57922e976
Gareth Pursehouse <gpursehouse@internetbrands.com>
2018-09-05 15:55:38 -0700

    upgrade to angular 6

--- resources/assets/src/app/providers/dashboard.service.ts
+++ resources/assets/src/app/providers/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import 'rxjs/add/operator/toPromise';
+// import 'rxjs/add/operator/toPromise';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
